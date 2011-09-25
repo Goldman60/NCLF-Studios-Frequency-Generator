d748e7f0e72c64f41b5fc9b37e200a2f4b0b0388
AJ Fite <austinjfite@hotmail.com>
2011-09-25 14:42:50 -0700

    First code push, build 1.0 final

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace F_G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IntervalDomain = new System.Windows.Forms.NumericUpDown();
+            this.FreqDomain = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DurationDomain = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalDomain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FreqDomain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DurationDomain)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Beeping";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(154, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Beeping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Beep Interval (Milliseconds)";
+            // 
+            // IntervalDomain
+            // 
+            this.IntervalDomain.Location = new System.Drawing.Point(213, 31);
+            this.IntervalDomain.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.IntervalDomain.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.IntervalDomain.Name = "IntervalDomain";
+            this.IntervalDomain.Size = new System.Drawing.Size(65, 20);
+            this.IntervalDomain.TabIndex = 4;
+            this.IntervalDomain.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // FreqDomain
+            // 
+            this.FreqDomain.Location = new System.Drawing.Point(213, 57);
+            this.FreqDomain.Maximum = new decimal(new int[] {
+            32767,
+            0,
+            0,
+            0});
+            this.FreqDomain.Minimum = new decimal(new int[] {
+            37,
+            0,
+            0,
+            0});
+            this.FreqDomain.Name = "FreqDomain";
+            this.FreqDomain.Size = new System.Drawing.Size(65, 20);
+            this.FreqDomain.TabIndex = 6;
+            this.FreqDomain.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Beep Frequency";
+            // 
+            // DurationDomain
+            // 
+            this.DurationDomain.Location = new System.Drawing.Point(213, 83);
+            this.DurationDomain.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.DurationDomain.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.DurationDomain.Name = "DurationDomain";
+            this.DurationDomain.Size = new System.Drawing.Size(65, 20);
+            this.DurationDomain.TabIndex = 8;
+            this.DurationDomain.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Beep Duration (milliseconds)";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(290, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 155);
+            this.Controls.Add(this.DurationDomain);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FreqDomain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IntervalDomain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "NCLF Frequency Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalDomain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FreqDomain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DurationDomain)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown IntervalDomain;
+        private System.Windows.Forms.NumericUpDown FreqDomain;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown DurationDomain;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace F_G
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            T = new Timer();
+            T.Tick += new EventHandler(T_Tick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            (sender as Button).Enabled = false;
+            ChangeEnables(false);
+            T.Interval = (int)(IntervalDomain.Value);
+            T.Start();
+            button2.Enabled = true;
+        }
+
+        Timer T;
+
+        void T_Tick(object sender, EventArgs e)
+        {
+            Console.Beep((int)FreqDomain.Value, (int)DurationDomain.Value);
+        }
+
+        void ChangeEnables(bool enabled)
+        {
+            DurationDomain.Enabled = enabled;
+            FreqDomain.Enabled = enabled;
+            IntervalDomain.Enabled = enabled;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            ChangeEnables(true);
+            T.Stop();
+            button1.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string a = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            MessageBox.Show(string.Format("NCLF Studios Frequency Generator v. {0}\nCopyright NCLF Studios 2011", a), "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
